6d8ffe63b97b794e5fbf32d5375986339e509aef
Jiang <caneliean@gmail.com>
2023-04-20 14:24:44 +0700

    modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
+import ClientOnly from "./components/clientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata = {
   title: "Modumart",
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div className="text-rose-500 text-2xl">Hello Modumart is buiding</div>
-  );
+  return <div className="text-rose-500 text-2xl">Wellcome To ModuMart</div>;
 }
