24333c01601176f31240dbefd332bb123b264d97
Vasin Suksuchit <vasinsuk@gosoft.co.th>
2023-05-02 17:23:05 +0700

    heart Button

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import { SafeUser } from "../types";
+
+import useFavorite from "@/app/hooks/useFavorite";
+import { SafeUser } from "@/app/types";
+
+import ClientOnly from "./ClientOnly";
 
 interface HeartButtonProps {
   listingId: string;
@@ -12,8 +16,11 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
+
   return (
     <div
       onClick={toggleFavorite}
@@ -22,16 +29,16 @@ const HeartButton: React.FC<HeartButtonProps> = ({
         hover:opacity-80
         transition
         cursor-pointer
-        "
+      "
     >
       <AiOutlineHeart
         size={28}
         className="
-                fill-white
-                absolute
-                -top-[2px]
-                -right-[2px]
-                "
+          fill-white
+          absolute
+          -top-[2px]
+          -right-[2px]
+        "
       />
       <AiFillHeart
         size={24}
--- app/hooks/useFavorite.ts
+++ app/hooks/useFavorite.ts
@@ -1,67 +1,59 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
-import { toast } from "react-hot-toast"
+import { toast } from "react-hot-toast";
 
-import { SafeUser } from "../types";
+import { SafeUser } from "@/app/types";
 
 import useLoginModal from "./useLoginModal";
 
-
-interface IUserFavorite {
-    listingId: string;
-    currentUser?: SafeUser | null;
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null;
 }
 
-const userFavorite = ({
-    listingId,
-    currentUser
-}: IUserFavorite) => {
-    const router = useRouter();
-    const loginModal = useLoginModal();
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
 
-    const hasFavorited = useMemo(() => {
-        const list = currentUser?.favoriteIds || [];
+  const loginModal = useLoginModal();
 
-        return list.includes(listingId);
-    }, [currentUser, listingId]);
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
 
-    const toggleFavorite = useCallback(async (
-        e: React.MouseEvent<HTMLDivElement>
-    ) => {
-        e.stopPropagation();
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
 
-        if (!currentUser) {
-            return loginModal.onOpen();
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+
+        if (hasFavorited) {
+          request = () => axios.delete(`/api/favorites/${listingId}`);
+        } else {
+          request = () => axios.post(`/api/favorites/${listingId}`);
         }
 
-        try {
-            let request;
+        await request();
+        router.refresh();
+        toast.success("Success");
+      } catch (error) {
+        toast.error("Something went wrong.");
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal, router]
+  );
 
-            if (hasFavorited) {
-                request = () => axios.delete(`/api/favorites/${listingId}`);
-            } else {
-                request = () => axios.post(`/api/favorites/${listingId}`);
-            }
-            await request();
-            router.refresh();
-            toast.success("Success");
-        } catch (error) {
-            toast.error("Something went wrong.");
-        }
-    }, [
-        currentUser,
-        hasFavorited,
-        listingId,
-        loginModal,
-        router
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
 
-    ]);
-
-    return {
-        hasFavorited,
-        toggleFavorite
-}
-}
-
-export default userFavorite;
+export default useFavorite;
